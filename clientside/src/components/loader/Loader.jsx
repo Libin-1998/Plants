635cf02e913ae02a849bd36a44913fa81cbb527e
Libin-1998 <libinninteen98@gmail.com>
2024-09-04 11:54:37 +0530

    loaderupdate

--- clientside/src/components/loader/Loader.jsx
+++ clientside/src/components/loader/Loader.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import './Loader.css'
-// import RingLoader from "react-spinners/SyncLoader";
+import React from 'react'
+import './Loader.css'
+import RingLoader from "react-spinners/SyncLoader";
 
-// export default function Loader() {
+export default function Loader() {
 
-//     const online=navigator.onLine
+    const online=navigator.onLine
 
-//   return (
-//     <>
-//     <div className='loaderpage'>
-//     <RingLoader
-//   color="#20b264"
-//   loading
-//   speedMultiplier={1}
-// />
-// </div>
-//     </>
-//   )
-// }
+  return (
+    <>
+    <div className='loaderpage'>
+    <RingLoader
+  color="#20b264"
+  loading
+  speedMultiplier={1}
+/>
+</div>
+    </>
+  )
+}
